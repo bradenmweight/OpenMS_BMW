5f78f8789921f63472231029b7ad1a5ba2649cbc
bradenmweight <braden.m.weight@gmail.com>
2024-09-25 12:36:08 -0400

    Added second symmetry-breaking attempt to UHF

--- openms/qmc/trial.py
+++ openms/qmc/trial.py
@@ -49,11 +49,22 @@ class TrialWFBase(object):
                 mf.kernel()
             elif ( trial == "UHF" ):
                 # UHF -- BMW: Need to break symmetry of initial guess to get right solution
-                mf = scf.UHF(mol)
+                mf1 = scf.UHF(mol)
                 dm_alpha, dm_beta = mf.get_init_guess()
                 dm_beta[:2,:2] = 0 # BMW: Set some of the beta coefficients to zero to break alpha/beta symmetry
                 dm = (dm_alpha,dm_beta)
-                mf.kernel(dm) # BMW: Pass in modified initial guess
+                mf1.kernel(dm) # BMW: Pass in modified initial guess
+                mf2 = scf.UHF(mol)
+                dm_alpha, dm_beta = mf.get_init_guess()
+                dm_beta[:2,:2] = 0 # BMW: Set some of the beta coefficients to zero to break alpha/beta symmetry
+                dm = (dm_alpha,dm_beta)
+                mf2.kernel(dm) # BMW: Pass in modified initial guess
+
+                if ( mf1.e_tot < mf2.e_tot ): # BMW: Check which symmetry breaking works... H2 is mf1 but LiH is mf2
+                    mf = mf1
+                else:
+                    mf = mf2
+
             else:
                 print("No trial wavefunction selected. Defaulting to RHF.")
                 mf = scf.RHF(self.mol)
